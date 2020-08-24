ad2e66485b2ac66b482ac2baccb0bd94800ddb65
성현 <mingyov0807@gmail.com>
2020-08-25 02:21:26 +0900

    boot pay postman test complete

--- app.js
+++ app.js
@@ -16,13 +16,12 @@ app.use(cors());
 app.set('port', process.env.PORT ||8000);
 app.use(morgan('dev'));
 
-app.get('/', (req, res) =>{
-    res.send('hello!');
+app.post('/v1/payment/test/', (req, res) =>{
+	console.log(req.body);
+	
 })
 
 app.use(bodyParser.json());
-
-var Bootpay = require('./server_nodejs-master/lib/bootpay.js');
 
 BootpayRest.setConfig(
     '59a4d32b396fa607c2e75e00',
@@ -30,40 +29,31 @@ BootpayRest.setConfig(
 );
 BootpayRest.getAccessToken().then(function (response) {
 	if (response.status === 200) {
-		console.log(response.data.token);
+		console.log(response);
 	}
 });
-app.post('/check',(req, res)=>{
-BootpayRest.getAccessToken().then(function (tokenData) {
-    if (tokenData.status === 200) {
-        BootpayRest.verify('1234')
-            .then(function (data) {
-                console.log(data);
-            });
-    } else {
-        console.log('error!')
-    }
-});
-}
-)
-app.post('doubleCheck',(req, res)=>{
 
-	BootpayRest.getAccessToken().then(function (response) {
-		// Access Token을 발급 받았을 때
-		if (response.status === 200 && response.data.token !== undefined) {
-			BootpayRest.verify('[[ receipt_id ]]').then(function (_response) {
-				// 검증 결과를 제대로 가져왔을 때
-				if (_response.status === 200) {
-					// 원래 주문했던 금액이 일치하는가?
-					// 그리고 결제 상태가 완료 상태인가?
-					if (_response.data.price === price && _response.data.status === 1) {
-						// TODO: 이곳이 상품 지급 혹은 결제 완료 처리를 하는 로직으로 사용하면 됩니다.
-					}
-				}
-			});
-		}
-	});
-})
+
+// BootpayRest.setConfig(
+//     '59a4d32b396fa607c2e75e00',
+//     't3UENPWvsUort5WG0BFVk2+yBzmlt3UDvhDH2Uwp0oA='    
+// );
+
+// BootpayRest.getAccessToken().then(function (response) {
+// 	// Access Token을 발급 받았을 때
+// 	if (response.status === 200 && response.data.token !== undefined) {
+// 		BootpayRest.verify('5f43dfaa2225b4000750b932').then(function (_response) {
+// 			// 검증 결과를 제대로 가져왔을 때
+// 			if (_response.status === 200) {
+// 				// 원래 주문했던 금액이 일치하는가?
+// 				// 그리고 결제 상태가 완료 상태인가?
+// 				if (_response.data.price === price && _response.data.status === 1) {
+// 					// TODO: 이곳이 상품 지급 혹은 결제 완료 처리를 하는 로직으로 사용하면 됩니다.
+// 				}
+// 			}
+// 		});
+// 	}
+// });
 
 app.listen(app.get('port'),()=>{
     console.log(app.get('port'),'번 포트에서 대기 중');
